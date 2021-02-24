ea283e5454446d647467d089d4695d19a109bd05
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-02-24 18:32:29 -0300

    introduçao a javascript e logica de programaçao dia 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/Dia1/Dia1_ecercício2.js
@@ -0,0 +1,12 @@
+let base = 5;
+let altura = 8;
+let area = base * altura;
+console.log (area)
+
+let retanguloUm = 25
+let retanguloDois = 25
+let retanguloTres = 25
+let retanguloQuatro = 25
+
+let perimetro = retanguloUm+retanguloDois+retanguloTres+retanguloQuatro
+console.log (perimetro)
