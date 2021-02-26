7ed439b77a7dc33510c9fbe3a742d1a9cb28a1d8
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-02-26 07:10:45 -0300

    adiciona mais exercicios

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Dia2_exercício12.js
@@ -0,0 +1,6 @@
+let numeros = [];
+
+for (let index =0;index <= 25; index++){
+    numeros.push(index)
+}
+console.log(numeros)
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/Dia2_exercício13.js
@@ -0,0 +1,9 @@
+let numeros = [];
+
+for (let index =0;index <= 25; index++){
+    numeros.push(index)
+}
+
+for (let indice =0;indice <numeros.length;indice++){
+    console.log (numeros[indice] /2);
+}
