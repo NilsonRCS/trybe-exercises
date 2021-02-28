5a053e41d508eb881f0a295ebf59af550d8603e4
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-02-28 12:19:39 -0300

    exercicios do dia 4.3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/DIa3_exercício1.js
@@ -0,0 +1,9 @@
+let linha = 5;
+let asteriscos = '*';
+let linha2 = '';
+for (index =0; index <linha; index++){
+    linha2 = linha2 + asteriscos
+}
+for (indice =0;indice < linha; indice++){
+    console.log(linha2)
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/DIa3_exercício2.js
@@ -0,0 +1,8 @@
+ let base = 5;
+ let asterisco ='*';
+ let imputLine = '';
+
+ for (index = 0; index <= base; index += 1){
+     console.log(imputLine);
+     imputLine = imputLine + asterisco;
+ }
