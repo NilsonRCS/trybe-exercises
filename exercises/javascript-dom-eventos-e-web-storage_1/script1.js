28d076c4bb7719e4eec1d16c6303afbf84498f48
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-04 08:54:15 -0300

    exercicio 1

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/script1.js
@@ -0,0 +1,8 @@
+document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Sword of the Stranger";
+document.getElementById("page-title").style.color = "pink";
+document.getElementById("second-paragraph").innerText = "Um filme incrivel!";
+document.getElementById("second-paragraph").style.color = "green";
+document.getElementById("subtitle").innerText = "animação jp é d+";
+document.getElementById("subtitle").style.color = "blue";
---
+++ exercises/javascript-dom-eventos-e-web-storage_1/script2.js
@@ -0,0 +1,5 @@
+function changeText() {
+    let paragraph = document.getElementsByTagName('p')[1];
+    paragraph.innerHTML = "Criando minha própria Startup"
+  }
+  changeText();
