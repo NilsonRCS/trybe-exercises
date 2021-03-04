865f8d561c76ab47872397a74f5940b38fe79c22
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-04 08:56:41 -0300

    exercicio 2, chegado junto ao gabarito

--- exercises/javascript-dom-eventos-e-web-storage_1/script2.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script2.js
@@ -3,3 +3,9 @@ function changeText() {
     paragraph.innerHTML = "Criando minha própria Startup"
   }
   changeText();
+
+  function changeSquareToGreen() {
+    let squareYellow = document.getElementsByClassName('main-content')[0];
+    squareYellow.style.background = "green";
+  }
+  changeSquareToGreen();
